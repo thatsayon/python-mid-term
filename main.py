b2b15079f12c5200cc87da3f0d21724295a6a00a
Ashiqul Islam Ayon <ashiqulislamayon28@gmail.com>
2023-10-07 14:22:54 +0600

    Project Completed

--- main.py
+++ main.py
@@ -8,34 +8,46 @@ class Star_Cinema:
 
 class Hall:
     def __init__(self, rows, cols, hall_no):
-        self.seats = {} 
-        self.show_list = []
-        self.rows = rows
-        self.cols = cols
-        self.hall_no = hall_no
+        self.__seats = {} 
+        self.__show_list = []
+        self.__rows = rows
+        self.__cols = cols
+        self.__hall_no = hall_no
     
     def entry_show(self, id, movie_name, time):
         data = (id, movie_name, time)
-        self.show_list.append(data)
-        seats = [[0 for i in range(self.cols)] for j in range(self.rows)]
-        self.seats[id] = seats
+        self.__show_list.append(data)
+        seats = [[0 for i in range(self.__cols)] for j in range(self.__rows)]
+        self.__seats[id] = seats
     
     def book_seats(self, sid, row, col):
-        self.seats[sid][row-1][col-1] = 1
+        print("-"*20)
+        if row <= self.__rows and col <= self.__cols:
+            if self.__seats[sid][col-1][row-1] == 0:
+                self.__seats[sid][col-1][row-1] = 1
+                print(f"Seat ({row}, {col}) booked for show {sid}")
+            else:
+                print("This seat isn't available\nChoose another seat")
+        else:
+            print("Trying to access invalid seat")
 
     def view_show_list(self):
-        for data in self.show_list:
+        print("-"*20)
+        for data in self.__show_list:
             print(f"MOVIE NAME: {data[1]} SHOW ID: {data[0]} TIME: {data[2]}")
     
     def view_available_seats(self, sid):
-        for data in self.seats[sid]:
-            print(data)
+        print("-"*20)
+        if sid in self.__seats:
+            for data in self.__seats[sid]:
+                print(data)
+        else:
+            print("Enter a valid show id")
 
 hall1 = Hall(6, 6, 1)
 Star_Cinema.entry_hall(hall1)
-hall1.entry_show(111, "Jawan", "12:00 PM")
-hall1.entry_show(222, "Nun 2", "4:00 PM")
-print(hall1.show_list)
+hall1.entry_show(111, "Jawan", "07/10/2023 12:00 PM")
+hall1.entry_show(222, "Nun 2", "07/10/2023 4:00 PM")
 while True:
     print("-"*20)
     print("1. VIEW ALL SHOW TODAY \n2. VIEW AVAILABLE SEATS \n3. BOOK TICKET \n4. Exit")
@@ -49,8 +61,9 @@ while True:
     elif user_in == 3:
         sid = int(input("Show Id: "))
         nt = int(input("Number of Ticket?: "))
-        r = int(input("Enter Seat Row: "))
-        c = int(input("Enter Seat Col: "))
-        hall1.book_seats(sid, r, c)
+        for i in range(nt):
+            r = int(input("Enter Seat Row: "))
+            c = int(input("Enter Seat Col: "))
+            hall1.book_seats(sid, r, c)
     elif user_in == 4:
         break
